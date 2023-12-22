c8bfb7a6ee18fe8f244ac414432409bc0ddf75d1
Anton <ulmananton@gmail.com>
2023-12-22 20:00:16 +0100

    db setup and basic html

---
+++ process_inventory.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "your_database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// hier berechnest du die inventur und alles andere und speicherst das in die datenbank. have fun
